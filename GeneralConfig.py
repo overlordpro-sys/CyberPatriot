7d6c3153343a9e9fa6f7f47b01cc30fd36668b89
overlordpro-sys <brent.littlebean@gmail.com>
2023-11-04 10:13:21 -0700

    check if 10-network-security... exist first before change config

--- GeneralConfig.py
+++ GeneralConfig.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import os
 from LoggerClass import Logger
 from Util import backup_then_clean_file
 
@@ -7,11 +7,13 @@ from Util import backup_then_clean_file
 def general_config(logger: Logger):
     logger.logH1("GENERAL CONFIG")
     # enable ipv4, ipv6 hardening
-    backup_then_clean_file(abs_path="/etc/sysctl.conf", file_name="sysctl.conf")
-    logger.logChange(f"Clean sysctl.conf")
+    if os.path.exists("/etc/sysctl.conf"):
+        backup_then_clean_file(abs_path="/etc/sysctl.conf", file_name="sysctl.conf")
+        logger.logChange(f"Clean sysctl.conf")
 
-    backup_then_clean_file(abs_path="/etc/sysctl.d/10-network-security.conf", file_name="10-network-security.conf")
-    logger.logChange(f"Clean 10-network-security.conf")
+    if os.path.exists("/etc/sysctl.d/10-network-security.conf"):
+        backup_then_clean_file(abs_path="/etc/sysctl.d/10-network-security.conf", file_name="10-network-security.conf")
+        logger.logChange(f"Clean 10-network-security.conf")
 
     subprocess.call("sysctl --system", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
