7f643531413c527ce59c40990aa1141beab407bb
overlordpro-sys <brent.littlebean@gmail.com>
2023-11-04 10:35:49 -0700

    Added sshd hardening

---
+++ SSHDHarden.py
@@ -0,0 +1,13 @@
+import os
+from LoggerClass import Logger
+from Util import backup_then_clean_file
+
+
+def sshd_harden():
+    if os.path.exists("/etc/ssh/sshd_config"):
+        backup_then_clean_file(abs_path="/etc/ssh/sshd_config", file_name="sshd_config")
+        logger.logChange(f"Clean sshd_config")
+
+
+if __name__ == "__main__":
+    sshd_harden()
