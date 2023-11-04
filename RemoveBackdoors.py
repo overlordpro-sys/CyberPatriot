1fa9e45549ed9fd50151539c27ab9deb34af9391
overlordpro-sys <brent.littlebean@gmail.com>
2023-11-04 10:19:19 -0700

    Added ports to remove backdoors

--- RemoveBackdoors.py
+++ RemoveBackdoors.py
@@ -10,15 +10,22 @@ def remove_backdoors(logger: Logger):
     logger.logH1("BACKDOORS")
 
     logger.logH2("Remove netcat backdoors")
-    for proc in psutil.process_iter(['name', 'pid', 'exe']):
-        if proc.name() == "nc" or proc.name() == "netcat" or proc.name() == "ncat":
+    for proc in psutil.process_iter(['name', 'pid', 'exe', 'connections']):
+        if proc.name() in ["nc", "netcat", "ncat"]:
             name = proc.info['name']
             pid = str(proc.info['pid'])
             exe = proc.info['exe']
+            ports = []
+            # Iterate over each connection object and extract the port
+            for conn in proc.connections():
+                if conn.laddr:
+                    ports.append(str(conn.laddr.port))
+            ports_str = ', '.join(ports)  # Create a string from the list of ports
             subprocess.call(str("cp " + exe + " backups/" + exe.rsplit('/')[-1]), shell=True)
             subprocess.call("rm " + exe, shell=True)
             subprocess.call("kill -9 " + pid, shell=True)
-            logger.logChange(f"Removed and backed up {name} - {exe}")
+            # Include the ports in the log
+            logger.logChange(f"Removed and backed up {name} (PID: {pid}) - {exe} - Ports: {ports_str}")
     logger.logHEnd()
 
     logger.logH2("Remove cronjobs")
